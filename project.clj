074ee004d1ed0b55b4cb2b1283b332a907d81605
George G <georgegatuma.gg@gmail.com>
2022-04-30 03:23:19 +0300

    add dockerfile

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :main ^:skip-aot github-axn-app.core
+  :uberjar-name "app.jar"
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
